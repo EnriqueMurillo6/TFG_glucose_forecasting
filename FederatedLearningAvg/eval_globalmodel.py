371deefe360d806f71164694d9c117e1a7624ed7
EnriqueMurillo <92783587+EnriqueMurillo6@users.noreply.github.com>
2025-06-13 19:23:33 +0200

    Update eval_globalmodel.py

--- FederatedLearningAvg/eval_globalmodel.py
+++ FederatedLearningAvg/eval_globalmodel.py
@@ -5,4 +5,7 @@ from scipy.stats import pearsonr
 from pytorch_forecasting import TimeSeriesDataSet
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-def load_data_patient_test(archivo_id
+def load_data_patient_test(df, archivo_id):
+  df_test = df[df["archivo_id"] == archivo_id].copy()
+  
+    
