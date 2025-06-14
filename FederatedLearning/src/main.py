4138df13f56d7d4d070dc21e77c06ecadd366755
EnriqueMurillo <92783587+EnriqueMurillo6@users.noreply.github.com>
2025-06-14 13:18:38 +0200

    Create main.py

---
+++ FederatedLearning/src/main.py
@@ -0,0 +1,18 @@
+from server import server
+from client import client
+from flwr import run_simulation
+from src.constants import DEVICE, NUM_CLIENTS  
+
+
+backend_config = {"client_resources": {"num_cpus": 1, "num_gpus": 0.0}}
+
+if DEVICE == "cuda":
+    backend_config = {"client_resources": {"num_cpus": 1, "num_gpus": 1.0}}
+
+if __name__ == "__main__":
+    run_simulation(
+        server_app=server,
+        client_app=client,
+        num_supernodes=NUM_CLIENTS,
+        backend_config=backend_config,
+    )
