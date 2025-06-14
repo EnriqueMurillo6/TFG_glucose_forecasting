95081693e61dadca53a43856acb4c713dca691ba
EnriqueMurillo <92783587+EnriqueMurillo6@users.noreply.github.com>
2025-06-14 12:57:34 +0200

    Delete FederatedLearningAvg directory

--- FederatedLearningAvg/config.py
+++
@@ -1,9 +0,0 @@
-# Parámetros generales
-BATCH_SIZE = 256
-LEARNING_RATE = 0.001
-MAX_ENCODER_LENGTH = 12
-MIN_ENCODER_LENGTH = 6
-MAX_PREDICTION_LENGTH = 6
-
-# Hiperparámetros federados
-NUM_ROUNDS = 10  # Número de rondas de aprendizaje federado
