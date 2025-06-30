5b36aa36eefc37850dcabb6f14864ec3fe6a0b5a
EnriqueMurillo <92783587+EnriqueMurillo6@users.noreply.github.com>
2025-06-30 16:54:02 +0200

    Delete FederatedLearning/src directory

--- FederatedLearning/src/data_loader.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_full_dataframe(path: str) -> pd.DataFrame:
-    df = pd.read_parquet(path)
-    df = df[df['archivo_id'].isin(df['archivo_id'].unique()[:20])]
-    return df
